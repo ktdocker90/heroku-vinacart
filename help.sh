c538454fcbcec1c4601cdae072fe6d619855ad94
Your Name <you@example.com>
2017-01-21 05:09:38 +0000

    commit the change

--- help.sh
+++ help.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #heroku config:set BUILDPACK=""
-heroku buildpacks:set https://github.com/site5/heroku-buildpack-php-with-ioncube
+heroku buildpacks:set https://github.com/ktdocker90/heroku-buildpack-php-vinacart
+git push -u origin master
