087aeb07badb3266ba4b0adc3ef03d845f9e6bda
Your Name <you@example.com>
2017-01-21 07:13:20 +0000

    commit the change

--- help.sh
+++ help.sh
@@ -9,3 +9,10 @@ git push heroku master
 #_------------
 nginx -t
 /app/.heroku/php/
+vendor/bin/heroku-php-nginx --verbose public/
+
+#nginx
+/app/.heroku/php/etc/php/php-fpm.conf
+/app/.heroku/php/etc/nginx/nginx.conf
+/app/vendor/heroku/heroku-buildpack-php/conf/nginx/heroku.conf
+/app/vendor/heroku/heroku-buildpack-php/conf/nginx/default_include.conf
