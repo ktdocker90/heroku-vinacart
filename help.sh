0fe17adecfeaed33b3afa5b8937d587c60d95f51
Your Name <you@example.com>
2017-01-21 05:52:45 +0000

    commit the change

--- help.sh
+++ help.sh
@@ -2,6 +2,6 @@
 
 #heroku config:set BUILDPACK=""
 heroku buildpacks:set https://github.com/ktdocker90/heroku-buildpack-php-vinacart
-git add .
+git add -A
 git commit -m "commit the change"
 git push heroku master
