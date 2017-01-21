aceef2446e474d40b94b1c8bdbd7f5bf10f005d9
Your Name <you@example.com>
2017-01-21 05:13:31 +0000

    commit the change

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: vendor/bin/heroku-php-nginx --verbose
+web: vendor/bin/heroku-php-nginx --verbose public
--- help.sh
+++ help.sh
@@ -2,4 +2,6 @@
 
 #heroku config:set BUILDPACK=""
 heroku buildpacks:set https://github.com/ktdocker90/heroku-buildpack-php-vinacart
+git add .
+git commit -m "commit the change"
 git push -u origin master
